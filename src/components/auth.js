5a52f19c0a6be9a8f0b9617ba42be9c552b91a92
Matheus Parracho <mathparracho@gmail.com>
2023-05-05 22:30:01 +0100

    add google auth

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,5 +1,5 @@
-import { auth } from "../config/firebase.js";
-import { createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth, googleProvider } from "../config/firebase.js";
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from "react";
 
 export const Auth = () => {
@@ -13,6 +13,14 @@ export const Auth = () => {
         } 
         catch (err) 
         {
+          console.error(err);
+        }
+      };
+
+      const signInWithGoogle = async () => {
+        try {
+          await signInWithPopup(auth, googleProvider);
+        } catch (err) {
           console.error(err);
         }
       };
@@ -51,6 +59,7 @@ export const Auth = () => {
             onChange={(e) => setPassword(e.target.value)}
             />
             <button onClick={signIn}>Sign In</button>
+            <button onClick={signInWithGoogle}> Sign In With Google</button>
             <button onClick={logout}>Log Out</button>
         </div>
     );
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBUkqr62QmScETTBkV_kaNIhVSJMhr-G-0",
@@ -15,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
